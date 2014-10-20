885bda95c79b9310969ae948c2b0444c8742773f
LabKitty <labkittyx@gmail.com>
2014-10-20 10:39:57 +0300

    Update

---
+++ new_page.php
@@ -0,0 +1,12 @@
+<?php require "components/site-functions.php"; ?>
+<?php redirect_to('/index.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "components/html-head.php"; ?>
+</head>
+<body>
+  <h1>Tere hommikust!</h1>
+</body>
+</html>
